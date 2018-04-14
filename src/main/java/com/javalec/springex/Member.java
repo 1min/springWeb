e9c21ef6177b3570df938520a8c3cd1b96649816
1min <31214658+1min@users.noreply.github.com>
2018-04-14 22:03:19 +0900

    13강까지 정리안햇던거

---
+++ src/main/java/com/javalec/springex/Member.java
@@ -0,0 +1,24 @@
+package com.javalec.springex;
+
+public class Member {
+
+	
+	private String pw;
+	private String id;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	
+	
+	
+}
--- src/main/java/com/javalec/springex/MyController.java
+++ src/main/java/com/javalec/springex/MyController.java
@@ -1,8 +1,11 @@
 package com.javalec.springex;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -26,5 +29,47 @@ public class MyController {
 		return mv;
 		
 	}
+	
+	@RequestMapping("/confirm")//confirm으로 들어오면
+	public String confirm(HttpServletRequest request,Model model) {//HttpServletRequest객체는 request받아오기
+		String id=request.getParameter("id");
+		String pw=request.getParameter("pw");
+		
+		model.addAttribute("id",id);
+		model.addAttribute("pw",pw);
+		
+		
+		return "/confirm";//confirm으로 보내기
+		
+	}
+	
+	@RequestMapping("/checkid")
+	public String checkid(@RequestParam("id") String id,@RequestParam("pw")String pw,Model model) {
+		//@requestParam("id")String id는 String id = request.getParameter("id")와 같은역할
+		model.addAttribute("id",id);
+		model.addAttribute("pw",pw);
+		
+		return "/checkid";
+		
+	}
+	
+	@RequestMapping("/join")
+	public String join(Member member) {//짧게 파라미터 받아서 객체로 넘기기
+		
+		return "/join";	
+	}
+	
+//	@RequestMapping("/join")굳이 길게할려면 이렇게
+//	public String join(@RequestParam("id")String id,@RequestParam("pw")String pw,Model model) {
+//		
+	
+//	Member member = new Member();
+//	member.setId(id);
+//	member.setPw(pw);
+//	
+//	model.addAttribute("member",member);
+//		return "/join";	
+//	}
+	
 	
 }
